166e17765e1af305619d3b6a503d8e37f8691aad
akbarhps <akbarputrasrg@gmail.com>
2022-09-18 20:07:07 +0700

    add unique constraint

--- src/main/resources/db/migration/V20220918__Skema_Awal.sql
+++ src/main/resources/db/migration/V20220918__Skema_Awal.sql
@@ -1,29 +1,34 @@
 CREATE TABLE payment_provider
 (
-    id varchar(36),
-    primary key (id)
+    id   VARCHAR(36),
+    code VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
 );
+
+ALTER TABLE payment_provider
+    ADD CONSTRAINT payment_provider_unique_code UNIQUE (code);
 
 CREATE TABLE invoice_type
 (
-    id varchar(36),
-    primary key (id)
+    id VARCHAR(36),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE invoice
 (
-    id varchar(36),
-    primary key (id)
+    id VARCHAR(36),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE virtual_account
 (
-    id varchar(36),
-    primary key (id)
+    id VARCHAR(36),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE payment
 (
-    id varchar(36),
-    primary key (id)
+    id VARCHAR(36),
+    PRIMARY KEY (id)
 );
